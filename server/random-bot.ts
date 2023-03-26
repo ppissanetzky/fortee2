f90ad2a76076dc22708e4f6b61abf8010e7a9ec4
Pablo Pissanetzky <ppissanetzky@netflix.com>
2023-03-25 20:51:48 -0700

    Random bots now try to call the best trump when they're stuck and they make a feeble attempt at playing a decent bone.

--- server/random-bot.ts
+++ server/random-bot.ts
@@ -1,11 +1,52 @@
-import { Bid } from './core';
+import assert from 'node:assert';
+import { Bid, Bone, Trump } from './core';
+import { Status } from './driver';
 import { RandomPlayer } from './player';
 import getNextBotName from './bot-names';
+import { TrumpStats } from './bidder';
 
 export default class RandomBot extends RandomPlayer {
 
+    private bones?: Bone[];
+    private trump?: Trump;
+    private lead?: Bone;
+
     constructor() {
         super(getNextBotName());
+    }
+
+    draw({ bones } : { bones: Bone[] }): void {
+        this.bones = bones;
+    }
+
+    async call({ possible } : { possible: Trump[] }): Promise<Trump> {
+        assert(this.bones, 'I haz no bonez');
+        return TrumpStats.best(possible, this.bones);
+    }
+
+    trumpSubmitted({ from, trump } : { from: string, trump: Trump; }): void {
+        this.trump = trump;
+    }
+
+    async play({ possible } : { possible: Bone[] }): Promise<Bone> {
+        assert(this.trump, 'I haz no trumpz');
+        // The bot is leading
+        if (!this.lead) {
+            const [bone] = Bone.orderedForTrump(this.trump, possible);
+            return bone;
+        }
+        const [bone] = this.lead.ordered(this.trump, possible);
+        return bone;
+    }
+
+    playSubmitted({ from, bone } : { from: string, bone: Bone }): void {
+        if (!this.lead) {
+            this.lead = bone;
+        }
+    }
+
+    endOfTrick({ winner, points, status } : { winner: string, points: number, status: Status }): void {
+        this.lead = undefined;
     }
 }
 
