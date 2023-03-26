e16913467656100ce4f5c31065a192da5bad2940
Pablo Pissanetzky <ppissanetzky@netflix.com>
2023-03-26 00:42:30 -0700

    Added a status component so we can see what the other players are doing. UI mostly functional.

---
+++ site/components/status.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-card
+    v-if="value"
+    width="280"
+    height="180"
+  >
+    <v-sheet height="46" :color="color">
+      <h2 class="text-center white--text">
+        {{ value.name }}
+      </h2>
+      <v-progress-linear
+        v-if="thinking"
+        color="white"
+        indeterminate
+        height="8"
+        striped
+      />
+    </v-sheet>
+
+    <div>
+      <!-- <v-card-text>
+        <v-progress-linear
+          :active="thinking"
+          color="white"
+          indeterminate
+          rounded
+          height="6"
+        />
+      </v-card-text> -->
+      <div v-if="value.waitingForBid">
+        <h3 class="text-center">
+          Waiting for bid
+        </h3>
+      </div>
+      <div v-else>
+        <div class="text-center">
+          <div v-if="value.bid">
+            <span class="text-h5">{{ value.bid }}</span>
+            <span v-if="value.trump">on</span>
+            <span v-if="value.trump" class="text-h5">{{ value.trump }}</span>
+          </div>
+        </div>
+      </div>
+      <div v-if="value.waitingForTrump">
+        <h3 class="text-center">
+          Waiting for trump
+        </h3>
+      </div>
+      <div v-if="value.waitingForPlay">
+        <h3 class="text-center">
+          Thinking
+        </h3>
+      </div>
+      <v-card-actions v-if="value.play">
+        <v-img
+          :src="`${value.play}.png`"
+          contain
+          max-height="80"
+        />
+      </v-card-actions>
+    </div>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    // The "status" object
+    value: {
+      type: Object,
+      default: null
+    },
+    color: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+    thinking () {
+      return this.value && (
+        this.value.waitingForBid ||
+        this.value.waitingForTrump ||
+        this.value.waitingForPlay)
+    }
+  },
+
+  watch: {
+  },
+
+  created () {
+  },
+
+  methods: {
+  }
+}
+</script>
--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,79 +1,99 @@
 <template>
   <v-app>
     <v-main>
-      <v-card flat max-width="960px">
-        <v-item-group active-class="primary">
-          <v-container>
-            <v-row>
-              <v-col
-                v-for="n in 4"
-                :key="n"
-                cols="3"
-              >
-                <v-item v-slot="{ active, toggle }">
-                  <v-img
-                    :src="`${bones[n - 1]}.png`"
-                    contain
-                    @click="toggle"
+      <v-container>
+        <v-row>
+          <v-col cols="4" />
+          <v-col cols="4">
+            <Status v-model="top" color="secondary" />
+          </v-col>
+          <v-col cols="4" />
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <Status v-model="left" color="primary" />
+          </v-col>
+          <v-col cols="4" />
+          <v-col cols="4">
+            <Status v-model="right" color="primary" />
+          </v-col>
+        </v-row>
+        <!-- My stuff -->
+        <v-row>
+          <v-col cols="12">
+            <v-item-group active-class="primary">
+              <v-container>
+                <v-row>
+                  <v-col
+                    v-for="n in 4"
+                    :key="n"
+                    cols="3"
                   >
-                    <div v-if="active" ext-h2 flex-grow-1 text-center />
-                  </v-img>
-                </v-item>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="1" />
-              <v-col
-                v-for="n in 3"
-                :key="n"
-                cols="3"
-              >
-                <v-item v-slot="{ active, toggle }">
-                  <v-img
-                    :src="`${bones[n + 3]}.png`"
-                    contain
-                    @click="toggle"
+                    <v-item v-slot="{ active, toggle }">
+                      <v-img
+                        :src="`${bones[n - 1]}.png`"
+                        contain
+                        @click="toggle"
+                      >
+                      <div v-if="active" ext-h2 flex-grow-1 text-center></div>
+                      </v-img>
+                    </v-item>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="1" />
+                  <v-col
+                    v-for="n in 3"
+                    :key="n"
+                    cols="3"
                   >
-                    <div v-if="active" ext-h2 flex-grow-1 text-center />
-                  </v-img>
-                </v-item>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-item-group>
-        <v-container>
-          <v-row>
-            <v-col cols="4">
-              <v-textarea
-                v-model="text"
-                outlined
-                no-resize
-                readonly
-                rows="10"
-              />
-            </v-col>
-            <v-col cols="4">
-              <v-btn
-                v-for="choice in choices"
-                :key="choice"
-                class="ma-1"
-                @click="choose(choice)"
-              >
-                {{ choice }}
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
+                    <v-item v-slot="{ active, toggle }">
+                      <v-img
+                        :src="`${bones[n + 3]}.png`"
+                        contain
+                        @click="toggle"
+                      >
+                      <div v-if="active" ext-h2 flex-grow-1 text-center>
+                      </div>
+                      </v-img>
+                    </v-item>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-item-group>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-btn
+              v-for="choice in choices"
+              :key="choice"
+              class="ma-1"
+              @click="choose(choice)"
+            >
+              {{ choice }}
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 <script>
+import Status from '~/components/status.vue'
 export default {
+  components: { Status },
   data () {
     return {
       ws: undefined,
       joining: false,
+      table: [],
+      waitingForBid: undefined,
+      bids: {},
+      waitingForTrump: undefined,
+      trump: {},
+      waitingForPlay: undefined,
+      plays: {},
       text: '',
       bones: ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
       choices: [],
@@ -111,13 +131,20 @@ export default {
     }
   },
   computed: {
+    left () {
+      return this.status(0)
+    },
+    top () {
+      return this.status(1)
+    },
+    right () {
+      return this.status(2)
+    }
   },
-  watch: {
-  },
+  watch: {},
   mounted () {
   },
   methods: {
-
     send (type, message, ack) {
       if (this.ws) {
         this.ws.send(JSON.stringify({
@@ -127,25 +154,29 @@ export default {
         }))
       }
     },
-
     handleMessage (type, message, ack) {
       console.log(ack || '', type, message)
       switch (type) {
         case 'welcome':
           this.send('createGame')
           break
-
         case 'youEnteredGameRoom':
+          this.table = message.bots.map(name => ({
+            name
+          }))
           this.send('inviteBot', { fillRoom: true })
           break
-
         case 'enteredGameRoom':
+          this.table = message.bots.map(name => ({
+            name
+          }))
           if (message.full) {
             this.send('startGame')
           }
           break
-
         case 'startingHand':
+          this.bids = {}
+          this.trump = {}
           this.choices = ['Ready?']
           this.choose = () => {
             this.text = ''
@@ -153,40 +184,39 @@ export default {
             this.send('readyToStartHand', null, ack)
           }
           break
-
         case 'draw':
           this.bones = message.bones
           break
-
         case 'waitingForBid':
           this.text += `\nWaiting for ${message.from} to bid`
+          this.waitingForBid = message.from
           break
-
         case 'bid':
           this.text += '\nYour bid'
+          this.waitingForBid = undefined
           this.choices = message.possible
           this.choose = (bid) => {
             this.choices = []
             this.send('submitBid', { bid: `#bid:${bid}` }, ack)
           }
           break
-
         case 'bidSubmitted':
           this.text += `\n${message.from} bid ${message.bid}`
+          this.waitingForBid = undefined
+          this.bids[message.from] = message.bid
           break
-
         case 'reshuffle':
           this.text += '\nReshuffle'
+          this.bids = {}
           break
-
         case 'bidWon':
-          this.text += `\n${message.winner} wond the bid with ${message.bid}`
+          this.text += `\n${message.winner} won the bid with ${message.bid}`
+          this.bids = { [message.winner]: message.bid }
           break
-
         case 'waitingForTrump':
           this.text += `\nWaiting for ${message.from} to call trumps`
+          this.waitingForTrump = message.from
           break
-
         case 'call':
           this.text += '\nYou call trumps'
           this.choices = message.possible
@@ -195,15 +225,15 @@ export default {
             this.send('callTrump', { trump: `#trump:${trump}` }, ack)
           }
           break
-
         case 'trumpSubmitted':
           this.text += `\n${message.from} called trumps ${message.trump}`
+          this.waitingForTrump = undefined
+          this.trump = { [message.from]: message.trump }
           break
-
         case 'waitingForPlay':
           this.text += `\nWaiting for ${message.from} to play`
+          this.waitingForPlay = message.from
           break
-
         case 'play':
           this.text += '\nYour turn'
           this.choices = message.possible
@@ -213,22 +243,36 @@ export default {
             this.send('play', { bone: `#bone:${bone}` }, ack)
           }
           break
-
         case 'playSubmitted':
           this.text += `\n${message.from} played the ${message.bone}`
+          this.waitingForPlay = undefined
+          this.plays[message.from] = message.bone
           break
-
         case 'endOfTrick':
           this.text += `\n${message.winner} won the trick with ${message.points}`
+          this.plays = {}
           break
-
         case 'endOfHand':
           this.text += `\n${message.winner} wins the hand\nUS ${message.status.US.marks} THEM ${message.status.THEM.marks}`
           break
-
         case 'gameOver':
           this.text = `\nGame over\nUS ${message.status.US.marks} THEM ${message.status.THEM.marks}`
           break
+      }
+    },
+    status (index) {
+      const name = this.table[index]?.name
+      if (!name) {
+        return {}
+      }
+      return {
+        name,
+        waitingForBid: this.waitingForBid === name,
+        bid: this.bids[name],
+        waitingForTrump: this.waitingForTrump === name,
+        trump: this.trump[name],
+        waitingForPlay: this.waitingForPlay === name,
+        play: this.plays[name]
       }
     }
   }
