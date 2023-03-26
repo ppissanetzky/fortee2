e79d677480eb9f3a38c269142946052b8f9d9e3b
Pablo Pissanetzky <ppissanetzky@netflix.com>
2023-03-26 00:41:41 -0700

    Added a delay to bot responses so I can see the UI

--- server/random-bot.ts
+++ server/random-bot.ts
@@ -1,4 +1,5 @@
 import assert from 'node:assert';
+import _ from 'lodash';
 import { Bid, Bone, Trump } from './core';
 import { Status } from './driver';
 import { RandomPlayer } from './player';
@@ -15,12 +16,24 @@ export default class RandomBot extends RandomPlayer {
         super(getNextBotName());
     }
 
+    protected delay(): Promise<void> {
+        return new Promise((resolve) => {
+            setTimeout(resolve, _.random(1500, 3500));
+        });
+    }
+
+    startingHand(): Promise<void> {
+        this.debug('starting hand');
+        return this.delay();
+    }
+
     draw({ bones } : { bones: Bone[] }): void {
         this.bones = bones;
     }
 
     async call({ possible } : { possible: Trump[] }): Promise<Trump> {
         assert(this.bones, 'I haz no bonez');
+        await this.delay();
         return TrumpStats.best(possible, this.bones);
     }
 
@@ -30,6 +43,7 @@ export default class RandomBot extends RandomPlayer {
 
     async play({ possible } : { possible: Bone[] }): Promise<Bone> {
         assert(this.trump, 'I haz no trumpz');
+        await this.delay();
         // The bot is leading
         if (!this.lead) {
             const [bone] = Bone.orderedForTrump(this.trump, possible);
@@ -54,6 +68,7 @@ export class PassBot extends RandomBot {
 
     async bid({ possible } : { possible: Bid[] }): Promise<Bid> {
         const [lowest] = possible;
+        await this.delay();
         return lowest;
 //        return super.bid({possible: [lowest]});
     }
