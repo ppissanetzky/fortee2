2530e42dbf1c95b3cc1a991ed139a181ad7184af
Pablo Pissanetzky <ppissanetzky@netflix.com>
2023-03-27 09:42:53 -0700

    Added score, teams and trick status

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -3,7 +3,41 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col cols="4" />
+          <v-col cols="4">
+            <v-card flat width="260">
+              <v-container>
+                <v-row>
+                  <v-col cols="8">
+                    <strong>US</strong>
+                    <br>
+                    <span class="caption">{{ me.name }} & {{ top.name }}</span>
+                  </v-col>
+                  <v-col>
+                    <h3>{{ US.marks }}</h3>
+                    <span class="caption">{{ US.points }}</span>
+                    <span v-if="bidForTeam('US')" class="caption">/ {{ bidForTeam('US') }}</span>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12">
+                    <v-divider />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8">
+                    <strong>THEM</strong>
+                    <br>
+                    <span class="caption">{{ left.name }} & {{ right.name }}</span>
+                  </v-col>
+                  <v-col>
+                    <h3>{{ THEM.marks }}</h3>
+                    <span class="caption">{{ THEM.points }}</span>
+                    <span v-if="bidForTeam('THEM')" class="caption">/ {{ bidForTeam('THEM') }}</span>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+          </v-col>
           <v-col cols="4">
             <Status v-model="top" color="secondary" />
           </v-col>
@@ -11,11 +45,11 @@
         </v-row>
         <v-row>
           <v-col cols="4">
-            <Status v-model="left" color="primary" />
+            <Status v-model="left" color="red darken-4" />
           </v-col>
           <v-col cols="4" />
           <v-col cols="4">
-            <Status v-model="right" color="primary" />
+            <Status v-model="right" color="red darken-4" />
           </v-col>
         </v-row>
         <v-row>
@@ -28,15 +62,20 @@
               flat
               width="280"
             >
-              <v-btn
+              <p v-if="choiceTitle">
+                {{ choiceTitle }}
+              </p>
+              <v-chip
                 v-for="choice in choices"
                 :key="choice"
                 small
-                class="ma-1"
+                label
+                color="primary"
+                class="mr-1 mb-1"
                 @click="choose(choice)"
               >
                 {{ choice }}
-              </v-btn>
+              </v-chip>
             </v-card>
           </v-col>
         </v-row>
@@ -98,9 +137,18 @@ export default {
     return {
       ws: undefined,
       joining: false,
+      US: {
+        marks: 0,
+        points: undefined
+      },
+      THEM: {
+        marks: 0,
+        points: undefined
+      },
       table: [],
       waitingForBid: undefined,
       bids: {},
+      bidWinner: undefined,
       waitingForTrump: undefined,
       trump: {},
       waitingForPlay: undefined,
@@ -108,6 +156,7 @@ export default {
       trickWinner: undefined,
       bones: ['null', 'null', 'null', 'null', 'null', 'null', 'null'],
 
+      choiceTitle: undefined,
       choices: [],
       choose: () => undefined
     }
@@ -126,9 +175,9 @@ export default {
     ws.onclose = (event) => {
       this.ws = undefined
     }
-    ws.addEventListener('error', (event) => {
+    ws.onerror = (event) => {
       console.log('error', event)
-    })
+    }
     ws.onopen = (event) => {
       this.ws = ws
       console.log('open')
@@ -144,22 +193,13 @@ export default {
     }
   },
   computed: {
-    left () {
-      return this.status(0)
-    },
-    top () {
-      return this.status(1)
-    },
-    right () {
-      return this.status(2)
-    },
-    me () {
-      return this.status()
-    }
+    left () { return this.status(0) },
+    top () { return this.status(1) },
+    right () { return this.status(2) },
+    me () { return this.status() }
   },
   watch: {},
-  mounted () {
-  },
+  mounted () {},
   methods: {
     send (type, message, ack) {
       if (this.ws) {
@@ -170,7 +210,7 @@ export default {
         }))
       }
     },
-    handleMessage (type, message, ack) {
+    async handleMessage (type, message, ack) {
       console.log(ack || '', type, message)
       switch (type) {
         case 'welcome':
@@ -193,11 +233,9 @@ export default {
         case 'startingHand':
           this.bids = {}
           this.trump = {}
-          this.choices = ['Ready?']
-          this.choose = () => {
-            this.choices = []
-            this.send('readyToStartHand', null, ack)
-          }
+          this.bidWinner = undefined
+          await this.prompt('Ready to start the next hand?', ['Yes'])
+          this.send('readyToStartHand', null, ack)
           break
         case 'draw':
           this.bones = message.bones
@@ -207,13 +245,11 @@ export default {
           break
         case 'bid':
           this.waitingForBid = 'you'
-          this.choices = message.possible
-          this.choose = (bid) => {
+          this.prompt('Your bid', message.possible).then((bid) => {
             this.waitingForBid = undefined
-            this.choices = []
             this.bids.you = bid
             this.send('submitBid', { bid: `#bid:${bid}` }, ack)
-          }
+          })
           break
         case 'bidSubmitted':
           this.waitingForBid = undefined
@@ -224,18 +260,19 @@ export default {
           break
         case 'bidWon':
           this.bids = { [message.winner]: message.bid }
+          this.bidWinner = message.winner
+          this.US.points = 0
+          this.THEM.points = 0
           break
         case 'waitingForTrump':
           this.waitingForTrump = message.from
           break
         case 'call':
           this.waitingForTrump = 'you'
-          this.choices = message.possible
-          this.choose = (trump) => {
+          this.prompt('Call trumps', message.possible).then((trump) => {
             this.trump = { you: trump }
-            this.choices = []
             this.send('callTrump', { trump: `#trump:${trump}` }, ack)
-          }
+          })
           break
         case 'trumpSubmitted':
           this.waitingForTrump = undefined
@@ -246,13 +283,11 @@ export default {
           break
         case 'play':
           this.waitingForPlay = 'you'
-          this.choices = message.possible
-          this.choose = (bone) => {
+          this.prompt('What will it be?', message.possible).then((bone) => {
             this.plays.you = bone
-            this.choices = []
             this.bones[this.bones.indexOf(bone)] = 'null'
             this.send('play', { bone: `#bone:${bone}` }, ack)
-          }
+          })
           break
         case 'playSubmitted':
           this.waitingForPlay = undefined
@@ -260,24 +295,38 @@ export default {
           break
         case 'endOfTrick':
           this.trickWinner = message.winner
-          this.choices = ['Next trick']
-          this.choose = () => {
-            this.plays = {}
-            this.trickWinner = undefined
-            this.choices = []
-            this.send('readyToContinue', null, ack)
-          }
+          this.US = message.status.US
+          this.THEM = message.status.THEM
+          await this.prompt(`${message.winner} won the trick with ${message.points} point${message.points === 1 ? '' : 's'}`, ['Next trick'])
+          this.plays = {}
+          this.trickWinner = undefined
+          this.send('readyToContinue', null, ack)
           break
         case 'endOfHand':
-          this.choices = ['Next hand']
-          this.choose = () => {
-            this.choices = []
+          {
+            const title = `${message.winner} won the hand`
+            this.US = message.status.US
+            this.THEM = message.status.THEM
+            await this.prompt(title, ['Continue'])
             this.send('readyToContinue', null, ack)
+            this.US.points = undefined
+            this.THEM.points = undefined
           }
           break
         case 'gameOver':
           break
       }
+    },
+    bidForTeam (team) {
+      if (this.bidWinner && this.teamFor(this.bidWinner) === team) {
+        return this.bids[this.bidWinner]
+      }
+    },
+    teamFor (name) {
+      if (name === this.me.name || name === this.top.name) {
+        return 'US'
+      }
+      return 'THEM'
     },
     status (index) {
       const name = typeof index === 'undefined' ? 'you' : this.table[index]?.name
@@ -294,6 +343,17 @@ export default {
         play: this.plays[name],
         trickWinner: this.trickWinner === name
       }
+    },
+    prompt (title, choices) {
+      this.choiceTitle = title
+      this.choices = choices
+      return new Promise((resolve) => {
+        this.choose = (value) => {
+          this.choiceTitle = undefined
+          this.choices = []
+          resolve(value)
+        }
+      })
     }
   }
 }
