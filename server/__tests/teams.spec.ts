5754af93afb02b7f95a8f421d3adbc0a3c819302
Pablo Pissanetzky <pablo@pissanetzky.com>
2023-04-13 17:42:04 -0700

    Making brackets!

--- server/__tests/teams.spec.ts
+++ server/__tests/teams.spec.ts
@@ -1,4 +1,5 @@
-import { jest, expect, test } from '@jest/globals';
+import { jest, expect, test, describe } from '@jest/globals';
+import _ from 'lodash';
 
 import * as db from '../tournament-db';
 
@@ -7,39 +8,221 @@ jest.mock('../tournament-db');
 const getSignups = jest.spyOn(db, 'getSignups');
 
 import Tournament from '../tournament';
-import TournamentDriver from '../tournament-driver';
+import TournamentDriver, { Team } from '../tournament-driver';
+import exp from 'constants';
 
-const tournament = new Tournament({
-    id: 1,
-    name: ``,
-    type: 1,
-    signup_start_dt: '',
-    signup_end_dt: '',
-    start_dt: '',
-    rules: '',
-    partner: 1,
-    seed: 0,
-    timezone: 'CST',
-    signup_opened: 0,
-    signup_closed: 0,
-    started: 0,
-    scheduled: 0,
-    finished: 0,
-    ladder_id: 0,
-    ladder_name: '',
-    lmdtm: '',
-    invitation: 0,
-    recurring: 0,
-    invitees: '',
-    prize: '',
-    winners: '',
-    recurring_source: 0,
-    host: ''
+describe('team counts', () => {
+    const tournament = new Tournament({
+        id: 1,
+        name: ``,
+        type: 1,
+        signup_start_dt: '',
+        signup_end_dt: '',
+        start_dt: '',
+        rules: '',
+        partner: 1,
+        seed: 0,
+        timezone: 'CST',
+        signup_opened: 0,
+        signup_closed: 0,
+        started: 0,
+        scheduled: 0,
+        finished: 0,
+        ladder_id: 0,
+        ladder_name: '',
+        lmdtm: '',
+        invitation: 0,
+        recurring: 0,
+        invitees: '',
+        prize: '',
+        winners: '',
+        recurring_source: 0,
+        host: ''
+    });
+
+    test.each([
+        /** player count, resulting number of teams, player dropped */
+        [0, 0, undefined],
+        [1, 0, '0'],
+        [2, 1, undefined],
+        [3, 1, expect.any(String)],
+        [4, 2, undefined],
+        [5, 2, expect.any(String)]
+
+    ])('with %d player(s) it should have %d team(s)', (count, teams, dropped) => {
+        const signups: string[] = [];
+        for (let i = 0; i < count; i++) {
+            signups.push(`${i}`);
+        }
+        expect(signups).toHaveLength(count);
+        expect(_.uniq(signups)).toHaveLength(count);
+        getSignups.mockReturnValueOnce(new Map(signups.map((item) => [item, null])));
+        const driver = new TournamentDriver(tournament);
+        expect(driver.teams).toHaveLength(teams);
+        expect(driver.dropped).toEqual(dropped);
+        const all: string [] = [
+            ...(driver.dropped ? [driver.dropped] : []),
+            ..._.flatten(driver.teams.map(({users}) => users))
+        ];
+        expect(all.sort()).toEqual(signups);
+    });
 });
 
-test('it works with no signups', () => {
-    getSignups.mockReturnValueOnce(new Map());
-    const driver = new TournamentDriver(tournament);
-    expect(driver.teams).toHaveLength(0);
-    expect(driver.dropped).toBeUndefined();
+describe('partner matching', () => {
+
+    const tournament = new Tournament({
+        id: 1,
+        name: ``,
+        type: 1,
+        signup_start_dt: '',
+        signup_end_dt: '',
+        start_dt: '',
+        rules: '',
+        partner: 2,
+        seed: 0,
+        timezone: 'CST',
+        signup_opened: 0,
+        signup_closed: 0,
+        started: 0,
+        scheduled: 0,
+        finished: 0,
+        ladder_id: 0,
+        ladder_name: '',
+        lmdtm: '',
+        invitation: 0,
+        recurring: 0,
+        invitees: '',
+        prize: '',
+        winners: '',
+        recurring_source: 0,
+        host: ''
+    });
+
+    type Signup = [string, string | null];
+    type Pair = [string, string];
+
+    const s = (a: string, b: string | null): Signup => [a, b];
+    const p = (a: string, b: string): Pair => [a, b];
+
+    test.each([
+        [
+            /** Neither picked a partner, but they get matched */
+            [s('a', null), s('b', null)],
+            [p('a', 'b')]
+        ],
+        [
+            /** A match made in heaven */
+            [s('a', 'b'), s('b', 'a')],
+            [p('a', 'b')]
+        ],
+        [
+            /** b and c picked each other, a gets dropped */
+            [s('a', 'b'), s('b', 'c'), s('c', 'b')],
+            [p('b', 'c')]
+        ],
+        [
+            /** a and b match */
+            [s('a', 'b'), s('c', 'a'), s('d', 'a'), s('b', 'a')],
+            [p('a', 'b'), p('c', 'd')]
+        ],
+
+    ])('match', (signups: Signup[], pairs: Pair[]) => {
+        getSignups.mockReturnValueOnce(new Map(signups));
+        const { teams } = new TournamentDriver(tournament);
+        expect(teams).toHaveLength(pairs.length);
+        expect(teams.map(({users}) => users.sort())).toEqual(pairs);
+    });
+});
+
+describe('brackets', () => {
+
+    const tournament = new Tournament({
+        id: 1,
+        name: ``,
+        type: 1,
+        signup_start_dt: '',
+        signup_end_dt: '',
+        start_dt: '',
+        rules: '',
+        partner: 1,
+        seed: 0,
+        timezone: 'CST',
+        signup_opened: 0,
+        signup_closed: 0,
+        started: 0,
+        scheduled: 0,
+        finished: 0,
+        ladder_id: 0,
+        ladder_name: '',
+        lmdtm: '',
+        invitation: 0,
+        recurring: 0,
+        invitees: '',
+        prize: '',
+        winners: '',
+        recurring_source: 0,
+        host: ''
+    });
+
+    const signups = (count: number): Map<string, string | null> => {
+        const result = new Map();
+        for (let i = 0; i < count; i++) {
+            result.set(`${i}`, null);
+        }
+        expect(result.size).toBe(count);
+        expect(_.uniq(Array.from(result.keys()))).toHaveLength(count);
+        return result;
+    };
+
+    test('returns empty with no signups', () => {
+        getSignups.mockReturnValueOnce(new Map());
+        const { games, rounds } = new TournamentDriver(tournament);
+        expect(games.size).toBe(0);
+        expect(rounds).toHaveLength(0);
+    });
+
+    test('validate 4 teams', () => {
+        getSignups.mockReturnValueOnce(signups(8));
+        const { games, rounds } = new TournamentDriver(tournament);
+        expect(games.size).toBe(3);
+        expect(rounds).toHaveLength(2);
+        for (const game of rounds[0]) {
+            expect(game.started).toBe(false);
+            expect(game.finished).toBe(false);
+            expect(game.teams).toHaveLength(2);
+            for (const team of game.teams) {
+                expect(team.users).toEqual(expect.arrayContaining(
+                    [expect.stringMatching(/\d/), expect.stringMatching(/\d/)]
+                ));
+            }
+            expect(game.previous_games).toHaveLength(0);
+            expect(game.next_game).toBe(rounds[1][0]);
+        }
+        expect(rounds[1]).toHaveLength(1);
+        const [game] = rounds[1];
+        expect(game.started).toBe(false);
+        expect(game.finished).toBe(false);
+        expect(game.teams).toHaveLength(0);
+        expect(game.previous_games).toHaveLength(2);
+        expect(game.previous_games[0]).toBe(rounds[0][0]);
+        expect(game.previous_games[1]).toBe(rounds[0][1]);
+        expect(game.next_game).toBeUndefined();
+    });
+
+    test('validate 5 teams', () => {
+        getSignups.mockReturnValueOnce(signups(10));
+        const { games, rounds } = new TournamentDriver(tournament);
+        expect(games.size).toBe(7);
+        expect(rounds).toHaveLength(3);
+        expect(rounds[0]).toHaveLength(4);
+        expect(rounds[1]).toHaveLength(2);
+        expect(rounds[2]).toHaveLength(1);
+        const byes = rounds[0].filter((game) => game.hasBye);
+        /** 3 of the 4 games in round 1 have byes */
+        expect(byes).toHaveLength(3);
+        /** Make sure no game has two byes */
+        const bad = rounds[0].filter(({teams}) =>
+            teams.every((team) => team === Team.bye));
+        expect(bad).toHaveLength(0);
+    });
 });
--- server/tournament-driver.ts
+++ server/tournament-driver.ts
@@ -2,8 +2,12 @@ import assert from 'node:assert';
 import _ from 'lodash';
 
 import Tournament from './tournament';
+import Dispatcher from './dispatcher';
 
 export class Team {
+
+    static bye = new Team('', '');
+
     public readonly users: string[];
 
     constructor(a: string, b: string) {
@@ -11,21 +15,78 @@ export class Team {
     }
 }
 
+export class Game {
+
+    public readonly id: number;
+    public readonly teams: Team[] = [];
+    public started = false;
+    public finished = false;
+    public col = -1;
+    public row = -1;
+    public readonly previous_games: Game[] = [];
+    public next_game?: Game;
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    get hasBye(): boolean {
+        return this.teams.includes(Team.bye);
+    }
+}
+
+export interface AnnounceBye {
+    user: string;
+    round: number;
+}
+
+export interface SummonPlayer {
+    user: string;
+    round: number;
+    gameRoomToken: string;
+}
+
+export interface TournamentDriverEvents {
+    /** Tell this user that they have a bye in this round */
+    announceBye: AnnounceBye;
+
+    /** Tell this player to come to this room */
+    summonPlayer: SummonPlayer;
+}
+
 export default class TournamentDriver {
 
     public readonly t: Tournament;
+
+    /** An array of player pairs in no particular order */
     public readonly teams: Team[];
+
+    /** If there was an odd number of players, one gets dropped */
     public readonly dropped: string | undefined;
+
+    /** All of the games by ID */
+    public readonly games: Map<number, Game>;
+
+    /** The rounds */
+    public readonly rounds: Game[][];
+
+    /** The current round */
+    public round = 0;
 
     constructor(t: Tournament) {
         this.t = t;
         [this.teams, this.dropped] = this.pickTeams();
+        [this.games, this.rounds] = this.createBracket();
     }
 
+    /**
+     * Pairs up all the signups and returns teams as well as one
+     * person who got dropped (or none)
+     */
+
     private pickTeams(): [Team[], string | undefined] {
-        /** Get the signups in a map of user/partner */
-        // TODO: randomize
-        const signups = this.t.signups();
+        /** Get the signups in a map of user/partner and shuffle it */
+        const signups = new Map(_.shuffle(Array.from(this.t.signups().entries())));
 
         /** Put all the players in a reject pile */
         const rejects = new Set(signups.keys());
@@ -91,4 +152,108 @@ export default class TournamentDriver {
         return [teams, dropped];
     }
 
+
+    private createBracket(): [Map<number, Game>, Game[][]] {
+
+        const { teams } = this;
+
+        const team_count = teams.length;
+
+        /** Not enough teams, return empty */
+        if (team_count < 4) {
+            return [new Map(), []];
+        }
+
+        let game_count = 2;
+
+        while (team_count > (2 * game_count)) {
+            game_count *= 2
+        }
+
+        let byes = (game_count * 2) - team_count;
+
+        const round_count = Math.floor(Math.log2(game_count)) + 1
+
+        const rounds: Game[][] = [];
+        const games = new Map<number, Game>();
+
+        for (let i = 0; i < round_count; i++) {
+            rounds.push([]);
+        }
+
+        for (let i = 0; i < game_count; i++) {
+            const game = new Game(i + 1);
+            rounds[0].push(game);
+            games.set(game.id, game);
+        }
+
+        function populate_game(index: number, round: number, byes: number, top: number, bottom: number) {
+            const game = rounds[round][index];
+            assert(game);
+            game.teams.push(teams[top]);
+            top += 1
+            if (byes > 0) {
+                game.teams.push(Team.bye);
+                byes -= 1;
+                game.started = true;
+                game.finished = true;
+            }
+            else {
+                game.teams.push(teams[bottom]);
+                bottom -= 1;
+            }
+            game.row = index * 2;
+            return [byes, top, bottom];
+        }
+
+        let top_team = 0;
+        let bot_team = teams.length - 1;
+
+        for (let i = 0; i < game_count / 2; i++) {
+            const top_bracket = i;
+            [byes, top_team, bot_team] =
+                populate_game(top_bracket, 0, byes, top_team, bot_team);
+            const bot_bracket = game_count - (1 + i);
+            [byes, top_team, bot_team] =
+                populate_game(bot_bracket, 0, byes, top_team, bot_team);
+        }
+
+        let round_games = game_count / 2;
+        let row = 1;
+        let game_id = game_count + 1;
+
+        for (let round = 2; round < round_count + 1; round++) {
+
+            const col = 2 * (round - 1);
+
+            for (let i = 0; i < round_games; i++) {
+
+                const game = new Game(game_id);
+
+                rounds[round - 1].push(game);
+
+                games.set(game.id, game);
+
+                const previous1 = rounds[round - 2][i * 2];
+                const previous2 = rounds[round - 2][(i * 2) + 1];
+
+                game.previous_games.push(previous1);
+                game.previous_games.push(previous2);
+
+                previous1.next_game = game;
+                previous2.next_game = game;
+
+                game.col = col;
+                game.row = row + ((game_count / round_games) * i * 2);
+
+                game_id += 1
+            }
+
+            row += Math.floor(Math.pow(2, round - 1));
+
+            round_games /= 2;
+        }
+
+        return [games, rounds];
+    }
 }
