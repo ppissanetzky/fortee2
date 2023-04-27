341a647c4b6026184019de434f47e00a1ce90fbe
Pablo Pissanetzky <pablo@pissanetzky.com>
2023-04-27 10:10:06 -0700

    Added 'off'

--- server/dispatcher.ts
+++ server/dispatcher.ts
@@ -15,6 +15,12 @@ export default class Dispatcher<T extends object> {
         return this;
     }
 
+    off<K extends keyof T>(
+        type: K, listener: (event: T[K]) => void) {
+        this.emitter.off(type as string, listener);
+        return this;
+    }
+
     once<K extends keyof T>(
         type: K, listener: (event: T[K]) => void) {
         this.emitter.once(type as string, listener);
